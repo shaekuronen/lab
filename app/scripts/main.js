ed94f5e2c76c888ca84c37416d8829e18025c5b9
Shae Kuronen <shae.kuronen@gmail.com>
2014-01-18 09:59:27 -0800

    initial commit of yeoman backbone generator assets

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global lab, $*/
+
+
+window.lab = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    lab.init();
+});
