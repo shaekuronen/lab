5093c94f4a148556b928b4aa3e6772e68181fd1d
Shae Kuronen <shae.kuronen@gmail.com>
2014-01-19 14:48:04 -0800

    updates to test/index.html and added experiment view and template

---
+++ app/scripts/views/experiment.js
@@ -0,0 +1,27 @@
+/*global lab, Backbone, JST*/
+
+lab.Views = lab.Views || {};
+
+(function () {
+    'use strict';
+
+    lab.Views.ExperimentView = Backbone.View.extend({
+
+        template: JST['app/scripts/templates/experiment.ejs'],
+
+        render: function() {
+          this.$el.html(template, this.model.toJSON());
+          return this;
+        },
+
+        events: {
+          'click #test-button': 'test_button_notifier'
+        },
+
+        test_button_notifier = function() {
+          console.log('Test button clicked');
+        }
+
+    });
+
+})();
---
+++ test/views/experiment.spec.js
@@ -0,0 +1,10 @@
+/*global describe, beforeEach, assert, it  */
+'use strict';
+
+describe('Experiment View', function () {
+
+    beforeEach(function () {
+        this.Experiment = new lab.Views.ExperimentView();
+    });
+
+});
