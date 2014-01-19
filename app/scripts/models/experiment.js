3540ab7114e59ca248413c888750dbcbac44745f
Shae Kuronen <shae.kuronen@gmail.com>
2014-01-18 16:30:54 -0800

    added experiment model

---
+++ app/scripts/models/experiment.js
@@ -0,0 +1,44 @@
+/*global lab, Backbone*/
+
+lab.Models = lab.Models || {};
+
+(function() {
+  'use strict';
+
+  lab.Models.ExperimentModel = Backbone.Model.extend({
+
+    defaults: {
+      published: false
+    },
+
+    initialize: function() {
+      console.log('This model has been initialized');
+      this.on('change', function () {
+        console.log('Values for this model have changed');
+
+        // console log any validation errors
+        this.on('invalid', function(model, error) {
+          console.log(error);
+        });
+      })
+    }
+
+    validate: function(attributes) {
+
+      // require a title for the experiement
+      if(attributes.title === undefined) {
+        return 'Please enter a title for the experiment';
+      }
+
+      // require a description for the experiment
+      if(attributes.description === undefined) {
+        return 'Please enter a description for the experiment';
+      }
+
+    },
+
+
+
+  });
+
+})();
---
+++ test/models/experiment.spec.js
@@ -0,0 +1,10 @@
+/*global describe, beforeEach, assert, it  */
+'use strict';
+
+describe('Experiment Model', function () {
+
+    beforeEach(function () {
+        this.Experiment = new lab.Models.ExperimentModel();
+    });
+
+});
